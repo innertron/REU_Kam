046188b0e2a7675289afcfca52e7971abdc80a5a
Kamal Kamalaldin <ask@kamalaldin.com>
2016-07-14 10:48:23 -0600

    Added scripts to parallelize the calculation of the ccm measure for surrogate data.

--- rEDM/for regions/calculate_signifigance_parallel.R
+++ rEDM/for regions/calculate_signifigance_parallel.R
@@ -1,8 +1,11 @@
+#this script produces surrogate data and calculates the ccm measure for
+#each such surrogate data
 #must run lump_channels_by_region.R first
 
 #import libraries
 library(eegkit)
 library(rEDM)
+library(snow)
 
 #for each brain region pair
 combinations <- t(combn(2:7, 2))
@@ -31,21 +34,7 @@ calc_pair_causality <- function(i, j)
   ch2_map_1_mean <- ccm_means(Ch2_xmap_Ch1)
   
   print(paste("finishe", i, j))
-  data.frame(to=j, from=i, libs=ch2_map_1_mean$lib_size,random_shuffle=NA, rho=ch2_map_1_mean$rho)
-}
-
-#calculate observed causality going one way
-observed <- rbind(
-  t(mcmapply(calc_pair_causality, combinations[,1], combinations[,2])),
-  t(mcmapply(calc_pair_causality, combinations[,2], combinations[,1]))
-)
-
-
-#make surrogte data
-surrogate.data <- data.frame(matrix(ncol=7, nrow=length(nd[,1])))
-surrogate.data[1] <- nd[,1]
-for(i in 2:7){
-  surrogate.data[,i] <- make_surrogate_data(nd[,i], method=shuffle_method)
+  c(to=j, from=i, libs=ch2_map_1_mean$lib_size,random_shuffle=NA, rho=ch2_map_1_mean$rho)
 }
 
 #make function to calculate surrogate data
@@ -62,12 +51,33 @@ calculate_pair_surrogate_causality <- function(i, j, surr.ind)
   ch2_map_1_mean <- ccm_means(Ch2_xmap_Ch1)
   
   print(paste("finished with ", i, j, surr.ind, "rho:", ch2_map_1_mean$rho))
-  data.frame(to=j, from=i, libs=ch2_map_1_mean$lib_size, random_shuffle=max(0, ch2_map_1_mean$rho), rho=NA)
+  c(to=j, from=i, libs=ch2_map_1_mean$lib_size, random_shuffle=max(0, ch2_map_1_mean$rho), rho=NA)
 }
 
-tick <- proc.time()
-#calculate ccm on surrogate data
-permutation_data <- t(mcmapply(calculate_pair_surrogate_causality, combinations[,1], combinations[,2], rep(1:100, each=length(combinations[,1]))))
-tock <- proc.time() - tick
+
+cl<-makeCluster(4 ,type="SOCK")
+
+clusterExport(cl, c("nd", "surrogate.data", 'simplex', 'lib', 'pred', 'ccm', 'ccm_means'))
+
+
+#calculate observed causality going one way
+observed <- rbind(
+  t(as.data.frame(clusterMap(cl, calc_pair_causality, combinations[,1], combinations[,2]))),
+  t(as.data.frame(clusterMap(cl, calc_pair_causality, combinations[,2], combinations[,1])))
+)
+
+
+#make surrogte data
+surrogate.data <- data.frame(matrix(ncol=7, nrow=length(nd[,1])))
+surrogate.data[1] <- nd[,1]
+for(i in 2:7){
+  surrogate.data[,i] <- make_surrogate_data(nd[,i], method=shuffle_method)
+}
+
+tick = proc.time()
+permutation_data <- t(as.data.frame(clusterMap(cl, calculate_pair_surrogate_causality, combinations[,1], combinations[,2], rep(1:100, each=length(combinations[,1])))))
+tock = proc.time() - tick
 print(tock)
+stopCluster(cl)
+
 permutation_test_data <- data.frame(rbind(permutation_data, observed))
